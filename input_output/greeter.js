720778c9bcd886558eaf275feea0409eb30e1e88
marymcdonald <10123446+marymcdonald@users.noreply.github.com>
2021-09-29 14:44:26 -0400

    Adding 3rd chapter exercises

---
+++ input_output/greeter.js
@@ -0,0 +1,11 @@
+let rlSync = require('readline-sync');
+/* for q1
+let name = rlSync.question("What is your name?\n");
+console.log(`Hello, ${name}!`);*/
+
+//for q2
+
+let first = rlSync.question("What is your first name?\n");
+let last = rlSync.question("What is your last name?\n");
+
+console.log(`Hello, ${first} ${last}!`);
